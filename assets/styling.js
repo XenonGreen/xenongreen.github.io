463992b55c1cd3e51a0393df41e15c62fd518b0f
willisman31 <58826505+willisman31@users.noreply.github.com>
2022-09-16 19:53:24 -0400

    Restructuring for Angular

--- assets/styling.js
+++
@@ -1,7 +0,0 @@
-function bottomizeBar() {
-    document.getElementById("footer").style.marginBlockEnd = '0px';
-}
-
-function upperizeBar() {
-    document.getElementsByName('header').style.marginBlockEnd = '0px';
-}
