6e7439cff55fbbc682fade2633c624bb4f29cc76
willisman31 <58826505+willisman31@users.noreply.github.com>
2022-09-24 19:38:19 -0400

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppModule } from './app.module';
+import { ComingSoonComponentComponent } from './coming-soon-component/coming-soon-component.component';
 
 const routes: Routes = [
-//  {path: '/', component: }
+//  {path: '', component: AppModule},
+  {path: 'Coming-Soon', component: ComingSoonComponentComponent}
 ];
 
 @NgModule({
