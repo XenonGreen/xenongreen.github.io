bd598bdea672eef1bddfbf6b9dbad94dc6a10ec2
willisman31 <58826505+willisman31@users.noreply.github.com>
2022-09-27 19:39:03 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppModule } from './app.module';
 import { ComingSoonComponentComponent } from './coming-soon-component/coming-soon-component.component';
 
 const routes: Routes = [
-//  {path: '', component: AppModule},
   {path: 'Coming-Soon', component: ComingSoonComponentComponent}
 ];
 
